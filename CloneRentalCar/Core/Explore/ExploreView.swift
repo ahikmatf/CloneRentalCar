d231c2cfad67d965b42495c5b24b163d33505f13
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-05-09 13:56:43 +0700

    project initialization

---
+++ CloneRentalCar/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  CloneRentalCar
+//
+//  Created by Asep Hikmat Fatahillah on 09/05/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Color.red.ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ CloneRentalCar/Core/TabBar/MainTabBarView.swift
@@ -0,0 +1,78 @@
+//
+//  MainTabBarView.swift
+//  CloneRentalCar
+//
+//  Created by Asep Hikmat Fatahillah on 09/05/24.
+//
+
+import SwiftUI
+
+struct MainTabBarView: View {
+    @State private var selectedTab: Int = 0
+    var body: some View {
+        TabView {
+            ExploreView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Explore")
+                    }
+                }
+                .onAppear {
+                    selectedTab = 0
+                }
+            Text("Favorite")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Explore")
+                    }
+                }
+                .onAppear {
+                    selectedTab = 1
+                }
+            Text("Trips")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "road.lanes")
+                            .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                        Text("Explore")
+                    }
+                }
+                .onAppear {
+                    selectedTab = 2
+                }
+            Text("Inbox")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "bubble")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Explore")
+                    }
+                }
+                .onAppear {
+                    selectedTab = 3
+                }
+            Text("More")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "ellipsis")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Explore")
+                    }
+                }
+                .onAppear {
+                    selectedTab = 4
+                }
+        }
+        .onAppear {
+            UITabBar.appearance().backgroundColor = .white
+        }
+    }
+}
+
+#Preview {
+    MainTabBarView()
+}
