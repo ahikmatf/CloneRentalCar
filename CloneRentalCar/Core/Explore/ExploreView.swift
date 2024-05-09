2f2ea2b41d2127d4ffe100d31ac29e2f997b6b75
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-05-09 16:04:29 +0700

    extract subview to different file

--- CloneRentalCar/Core/Explore/ExploreView.swift
+++ CloneRentalCar/Core/Explore/ExploreView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Color.red.ignoresSafeArea()
+        NavigationStack {
+            ScrollView {
+                ZStack {
+                    GradientColorBackground()
+                    Spacer()
+                        .frame(height: 150)
+                    CustomSearchBar()
+                }
+            }
+            .navigationStackModifier()
+        }
     }
 }
 
 #Preview {
     ExploreView()
+}
+
+struct GradientColorBackground: View {
+    var body: some View {
+        VStack {
+            LinearGradient(colors: [.gray, .blue], startPoint: .top, endPoint: .bottom)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
+            Spacer()
+        }
+    }
 }
