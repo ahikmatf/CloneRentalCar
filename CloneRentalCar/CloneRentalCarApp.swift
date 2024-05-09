cfd3695cd721e7b25ec7ac9cf1586b0d58e6deb9
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-05-09 10:43:08 +0700

    Initial Commit

---
+++ CloneRentalCar/CloneRentalCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneRentalCarApp.swift
+//  CloneRentalCar
+//
+//  Created by Asep Hikmat Fatahillah on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneRentalCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
